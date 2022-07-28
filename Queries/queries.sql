2624d1e25e705bac6c81368be6bdf94ce574c525
Brotherscodes <dylanbrothers@hotmail.com>
2022-07-28 18:43:32 -0400

    Updating queries.sql file

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -106,3 +106,57 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+-- Dropping table query info to incluide emp_no in results
+DROP TABLE retirement_info;
+
+
+-- Create new table for retiring employees to include emp_no in results:
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM "Employees"
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+
+-- Joining departments and dept_manager tables
+SELECT "Departments".dept_name,
+     "Managers".emp_no,
+     "Managers".from_date,
+     "Managers".to_date
+FROM "Departments"
+INNER JOIN "Managers"
+ON "Departments".dept_no = "Managers".dept_no;
+
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    "Dept_Emp".to_date
+FROM retirement_info
+LEFT JOIN "Dept_Emp"
+ON retirement_info.emp_no = "Dept_Emp".emp_no;
+
+-- Aliases for code readability:
+
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+	FROM retirement_info as ri
+LEFT JOIN "Dept_Emp" as de
+ON ri.emp_no = de.emp_no;
+
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN "Dept_Emp" as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
